b628058317b5595de28db96c4a6cab604b9dac05
Jordan Cregan <jpcregan@protonmail.com>
2025-09-05 14:24:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# hex
+
+A tool to assemble binary files from text. 
+
+## Requirements
+
+- C89 Compiler
+
+## Installation
+
+	$ cc hex.c -o hex
+	# cp hex /usr/local/bin
+
+## Usage
+
+`hex input output`
+
+## Example
+
+	$ cat input
+	b8 03 00
+	cd 10
+
+	$ hex input output
+
+	$ hd output
+	00000000  b8 03 00 cd 10                                    |.....|
+	00000005
+
+## Features
+
+- ### Comments
+		b8 03 00	; ax = 0x3
+		cd 10		; int 0x10
+
+- ### Set position
+		0x1fe		; boot
+		55 aa
+
+- ### String conversion
+		$ cat input
+		"Hello, World!"
+
+		$ hex input output
+
+		$ hd output
+		00000000  48 65 6c 6c 6f 2c 20 57  6f 72 6c 64 21           |Hello, World!|
+		0000000d
+
+## License
+
+Licensed under [0BSD](https://opensource.org/license/0BSD).
---
+++ hex.c
@@ -0,0 +1,152 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+	FILE *input, *output;
+
+	int c;
+	char s[3];
+
+	if (argc != 3)
+	{
+		fprintf(stderr, "usage: hex input output\n");
+		exit(EXIT_FAILURE);
+	}
+
+	input = fopen(argv[1], "r");
+	if (input == NULL)
+	{
+		fprintf(stderr,"Error opening file \"%s\": %s\n", argv[1],
+			strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	output = fopen(argv[2], "wb");
+	if (output == NULL)
+	{
+		fprintf(stderr, "Error opening file \"%s\": %s\n", argv[2],
+			strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	s[2] = 0;
+	for (;;)
+	{
+		c = fgetc(input);
+
+		if (c == EOF)
+			break;
+		
+		if (isspace(c))
+			continue;
+		
+		if (c == ';')
+		{
+			for (;;)
+			{
+				c = fgetc(input);
+
+				if (c == '\n' || c == EOF)
+					break;
+			}
+
+			continue;
+		}
+
+		if (c == '"')
+		{
+			for (;;)
+			{
+				c = fgetc(input);
+
+				if (c == EOF)
+				{
+					fprintf(stderr, "Unexpected EOF\n");
+					exit(EXIT_FAILURE);
+				}
+
+				if (c == '"')
+					break;
+
+				fputc(c, output);
+			}
+
+			continue;
+		}
+
+		if (c == '0')
+		{
+			c = fgetc(input);
+
+			if (c == 'x' || c == 'X')
+			{
+				long int pos, target;
+
+				pos = ftell(output);
+				if (pos == -1)
+				{
+					fprintf(stderr,
+						"Error reading position\n");
+					exit(EXIT_FAILURE);
+				}
+				
+				if (fscanf(input, "%lx", &target) != 1)
+				{
+					fprintf(stderr,
+						"Error scanning position\n");
+					exit(EXIT_FAILURE);
+				}
+
+				if (pos > target)
+				{
+					fprintf(stderr,
+						"Invalid position: 0x%lx\n",
+						target);
+					exit(EXIT_FAILURE);
+				}
+
+				if (fseek(output, target, SEEK_SET) != 0)
+				{
+					fprintf(stderr,
+						"Error setting position\n");
+					exit(EXIT_FAILURE);
+				}
+				continue;
+			}
+
+			ungetc(c, input);
+			c = '0';
+		}
+
+		if (!isxdigit(c))
+		{
+			fprintf(stderr,
+				"Unexpected character: %c\n",
+				c);
+			exit(EXIT_FAILURE);
+		}
+		s[0] = c;
+
+		c = fgetc(input);
+		if (!isxdigit(c))
+		{
+			fprintf(stderr,
+				"Unexpected character: %c\n",
+				c);
+			exit(EXIT_FAILURE);
+		}
+		s[1] = c;
+
+		fputc(strtol(s, 0, 16), output);
+	}
+
+	fclose(input);
+	fclose(output);
+
+	return EXIT_SUCCESS;
+}
